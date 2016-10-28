837cf52610a4ab5a16190cceb9cf721c29701256
hushaowei16 <34492710@qq.com>
2016-10-28 20:08:40 +0800

    Update ApplicationContextUtil.java
    添加注释

--- src/com/atguigu/p2p/util/ApplicationContextUtil.java
+++ src/com/atguigu/p2p/util/ApplicationContextUtil.java
@@ -5,11 +5,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 public class ApplicationContextUtil implements ApplicationContextAware {
-
+        
 	private static ApplicationContext applicationContext;
 
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+                //初始化容器对象
 		ApplicationContextUtil.applicationContext = applicationContext;
 	}
 
@@ -18,10 +19,12 @@ public class ApplicationContextUtil implements ApplicationContextAware {
 	}
 
 	public static Object getBean(String name) {
+                //根据id获取实例
 		return applicationContext.getBean(name);
 	}
 
 	public static <T> T getBean(Class<T> requiredType) {
+                //根据class获取实例
 		return applicationContext.getBean(requiredType);
 	}
 
